c4989df0d4d6a5f54afee7a0342015b9568b609c
Mahmudul Hasan <hasanmahmudul356@gmail.com>
2021-02-03 16:44:23 +0600

    Edist json
    Edist json

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var data = {
-    'header_name' : 'Mahmudul Hasan',
+    'header_name' : 'Mahmudul Hasan Edited',
     'about_my_self' : 'I\'m a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, ' +
         'I am passionate about building excellent software that improves the lives of those around me.'
 }
