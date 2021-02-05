53c66f798ffa4d523ddff5d1feffbc8475f83862
Mahmudul Hasan <hasanmahmudul356@gmail.com>
2021-02-05 22:42:41 +0600

    dd

--- js/vue_cli_render.js
+++ js/vue_cli_render.js
@@ -2,6 +2,12 @@ new Vue({
     el: '#Vue_component_wrapper',
     data: {
         DataList: [],
+        contactForm : {
+            name : '',
+            email : '',
+            subject : '',
+            message : '',
+        }
     },
     methods: {
         getValue: function (tag_name) {
@@ -9,6 +15,16 @@ new Vue({
                 return this.DataList[tag_name];
             }
         },
+        imageUrl : function (image_folder, image) {
+            console.log(image_folder);
+            console.log(image);
+            if (image_folder && image) {
+                return this.DataList.imageBasePath+image_folder+image;
+            }
+        },
+        submitForm : function () {
+            window.open('mailto:hasanmahmudul356@gmail.com?subject='+this.contactForm.subject+'&body='+this.contactForm.message+'&name='+this.contactForm.name);
+        }
     },
     created() {
         this.DataList = data;
